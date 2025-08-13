a29edd18938304724dd00113f7e11b2a6dff6372
Oliver Abreu <oliverabreu@icloud.com>
2025-08-13 15:58:26 -0400

    tries to prevent overscrolling and fixes header style

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header
-  class="flex items-center pt-8 pb-2 sticky top-0 bg-[var(--color-primary-green)] z-10 w-screen shadow-md px-8 sm:px-6"
+  class="flex items-center pt-4 pb-3 sticky top-0 bg-[var(--color-primary-green)] z-10 w-screen shadow-md px-8 sm:px-6"
 >
   <h1
     class="max-w-[30ch] mx-auto w-full text-white font-manrope text-3xl font-bold tracking-tight"
@@ -7,3 +7,10 @@
     <span class="tracking-tighter">HEAR</span> Journal
   </h1>
 </header>
+
+<style>
+  header {
+    padding-top: env(safe-area-inset-top);
+    background-color: #82d844; /* or your theme */
+  }
+</style>
